dbc02c082160d3f2094ebc0abba5888284a9ce57
drohiro <rodrigorfsfr3@gmail.com>
2019-11-25 17:36:56 -0300

    funcs

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,41 +1,33 @@
 // Aqui veremos em qual página o cliente está navegando
 window.onload = function () {
     var URL = window.location.href;
-    var provaUrlCheckout = URL.indexOf('checkout.html');
-    var provaUrlConfirmation = URL.indexOf('confirmation');
-    console.log(`checkout = ${provaUrlCheckout} =========== e confirmation = ${provaUrlConfirmation}`);
+    console.log('checkout = '+ URL.indexOf('checkout.html') + 
+    " <==============> confirmation = " + URL.indexOf('confirmation') );
+    
     
     if  (URL.indexOf('checkout') != -1 ) {
-        console.log (" Você está no Checkout pela URL = " + URL);
+        console.log (" Você está em: " + URL);
+        var valorTotal = 200;
+        
+        document.getElementById('nome').addEventListener('blur', function(event) {
+            console.log(event.target.parentElement.parentElement.children[0].innerText + ': ' + event.target.value);
+        });
 
 
 
+        // Altera Valor Total com formatação pt-BR:
+        document.getElementsByTagName('span')[8].innerHTML = valorTotal.toLocaleString('pt-BR', {style:'currency', 'currency': 'BRL'}); 
 
-
-
-
-
-
-        document.getElementsByClassName('main-btn standard-one').innerHTML
-   
+        // Aiciona link no botão "Finalizar compra"
+        document.getElementsByClassName('main-btn standard-one')[0].href = "./confirmation.html";         
 
 
     } else if (URL.indexOf('confirmation.html') != -1){
-        console.log (" Você está na Confirmação pela URL = " + URL);
-
-    } else { 
-        console.log('url invalida');
-    }
-
-}
-
-// redireciona para confirmation
+        console.log (" Você está em: " + URL);
 
 
-
-// Aqui começa o script de Checkout
-
-
-
-
-// Aqui começa o script de confirmation
+        // Adiciona numero de pedido ao console
+        console.log("Numero do " + (document.getElementsByClassName('header-title')[0].
+        textContent).toLocaleLowerCase())
+    }   
+} 
