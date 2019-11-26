7e4cd4aa31926f3922a9acf4095b95c6e496cb34
drohiro <rodrigorfsfr3@gmail.com>
2019-11-26 17:40:07 -0300

    fase 3

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,33 +6,30 @@ window.onload = function () {
     // todo: remover todos console.log que não esteja utilizando    
     
     if  (url.indexOf('checkout') != -1 ) {
-        
+
         // todo: criar uma variavel para os elementos, para não ficar repetindo código
-        var elementHeineken = document.getElementsByClassName('heineken');
-        var heineken = parseFloat(elementHeineken[1].innerHTML.replace('R$ ','')) * parseFloat(elementHeineken[0].children[0].innerHTML)       
+        var elementHeineken = document.getElementsByClassName('heineken'); 
+        var heineken = getElementValue(elementHeineken);    
         var elementCoruja = document.getElementsByClassName('coruja');
-        var coruja = parseFloat(elementCoruja[1].innerHTML.replace('R$ ','')) * parseFloat(elementCoruja[0].children[0].innerHTML)        
+        var coruja = getElementValue(elementCoruja);     
         var elementQuilmes = document.getElementsByClassName('quilmes');
-        var quilmes = parseFloat(elementQuilmes[1].innerHTML.replace('R$ ','')) * parseFloat(elementQuilmes[0].children[0].innerHTML)        
+        var quilmes = getElementValue(elementQuilmes);        
         var elementDeus = document.getElementsByClassName('deus');
-        var deus = parseFloat(elementDeus[1].innerHTML.replace('R$ ','')) * parseFloat(elementDeus[0].children[0].innerHTML)
+        var deus = getElementValue(elementDeus); 
         
         totalPedido = (heineken + coruja + quilmes + deus)          
 
         // Segue codigo que ira mostrar no console os inputs
         // todo: utilizar a função printData....
-        
-        function printData(event) {
-            console.log(event.target.parentElement.parentElement.children[0].innerText + ': ' + event.target.value);
-        }
-        document.getElementById('nome').addEventListener('blur', printData);                  // conteuno de nome      
-        document.getElementsByClassName('sobrenome')[0].addEventListener('blur', printData);  // conteudo de sobrenome
-        document.getElementsByTagName('input')[2].addEventListener('blur', printData);        // conteudo de CPF
-        document.getElementsByName('email')[0].addEventListener('blur', printData);           // conteudo de email
-        document.getElementsByTagName('input')[4].addEventListener('blur', printData);        // conteudo de telefone
-        document.getElementsByName('celular')[0].addEventListener('blur', printData);         // conteudo de celular
-        document.getElementById('cep').addEventListener('blur', printData);                   // conteudo de cep
-        document.getElementsByClassName('rua')[0].addEventListener('blur', printData);        // conteudo de rua
+
+        setBlurEventById('nome');                   // conteuno de nome      
+        setBlurEventByClassName('sobrenome', 0);    // conteudo de sobrenome
+        setBlurEventByTagName('input', 2);          // conteudo de CPF
+        setBlurEventByName('email', 0);             // conteudo de email
+        setBlurEventByTagName('input', 4);          // conteudo de telefone
+        setBlurEventByName('celular', 0);           // conteudo de celular
+        setBlurEventById('cep')                     // conteudo de cep
+        setBlurEventByClassName('rua', 0)           // conteudo de rua
         document.getElementsByTagName('input')[8].addEventListener('blur', printData);        // conteudo de bairro
         document.getElementsByName('cidade')[0].addEventListener('blur', printData);          // conteudo de cidade
         document.getElementsByTagName('input')[10].addEventListener('blur', printData);       // conteudo de estado
@@ -50,4 +47,27 @@ window.onload = function () {
         console.log("numero do " + (document.getElementsByClassName('header-title')[0].textContent).toLocaleLowerCase())        
     }   
 } 
- 
+
+function setBlurEventById(id) {
+    document.getElementById(id).addEventListener('blur', printData);    
+}
+
+function setBlurEventByClassName(classe, position) {
+    document.getElementsByClassName(classe)[position].addEventListener('blur', printData);
+}
+
+function setBlurEventByTagName(tagName, position) {
+    document.getElementsByTagName(tagName)[position].addEventListener('blur', printData);    
+}
+
+function setBlurEventByName (name, position) {
+    document.getElementsByName(name)[position].addEventListener('blur', printData);
+}
+
+function printData(event) {
+    console.log(event.target.parentElement.parentElement.children[0].innerText + ': ' + event.target.value);
+}
+
+function getElementValue(element) {
+    return parseFloat(element[1].innerHTML.replace('R$ ','')) * parseFloat(element[0].children[0].innerHTML);
+}
